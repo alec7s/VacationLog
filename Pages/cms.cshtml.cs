2bf829da5ef5077261af529d8a28b15653fca385
alec7s <alex.c.evans1@gmail.com>
2022-01-21 08:22:38 -0500

    Add attributes for form validation in CMS html and add fields to cmsModel

--- Pages/cms.cshtml.cs
+++ Pages/cms.cshtml.cs
@@ -5,7 +5,28 @@ namespace VacationLog.Pages
 {
     public class cmsModel : PageModel
     {
+        [BindProperty]
+        public string Title { get; set; }
+        public string Name { get; set; }
+
+        [BindProperty]
+        public int Year { get; set; }
+
+        [BindProperty]
+        public string Month { get; set; }
+
+        [BindProperty]
+        public string Description { get; set; }
+
+        public List<string> ImageFileNames { get; set; }
+        public List<string> ImageAltTexts { get; set; }
+        public List<string> ImageCaptions { get; set; }
+
         public void OnGet()
+        {
+        }
+
+        public void OnPost()
         {
         }
     }
